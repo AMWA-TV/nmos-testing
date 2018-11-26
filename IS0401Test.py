2fcf3de0188341512310df6bfd2c23e31a4ca73d
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-11-26 13:45:25 +0000

    Fix typo

--- IS0401Test.py
+++ IS0401Test.py
@@ -345,14 +345,14 @@ class IS0401Test(GenericTest):
 
                 receiver = response.json()
                 if receiver["subscription"]["sender_id"] != request_data["id"]:
-                    return test.FAIL("Node API Receiver subscription {} does not reflect the subscribed \
-                                      Sender ID", receiver["id"])
+                    return test.FAIL("Node API Receiver {} subscription does not reflect the subscribed " \
+                                     "Sender ID".format(receiver["id"]))
 
                 api = self.apis[NODE_API_KEY]
                 if api["major_version"] > 1 or (api["major_version"] == 1 and api["minor_version"] >= 2):
                     if not receiver["subscription"]["active"]:
-                        return test.FAIL("Node API Receiver subscription {} does not indicate an active \
-                                          subscription", receiver["id"])
+                        return test.FAIL("Node API Receiver {} subscription does not indicate an active " \
+                                         "subscription".format(receiver["id"]))
 
                 formats_tested.append(stream_type)
 
@@ -390,14 +390,14 @@ class IS0401Test(GenericTest):
 
                 receiver = response.json()
                 if receiver["subscription"]["sender_id"] is not None:
-                    return test.FAIL("Node API Receiver subscription {} does not reflect the subscribed \
-                                      Sender ID", receiver["id"])
+                    return test.FAIL("Node API Receiver {} subscription does not reflect the subscribed " \
+                                     "Sender ID".format(receiver["id"]))
 
                 api = self.apis[NODE_API_KEY]
                 if api["major_version"] > 1 or (api["major_version"] == 1 and api["minor_version"] >= 2):
                     if receiver["subscription"]["active"]:
-                        return test.FAIL("Node API Receiver subscription {} does not indicate an inactive \
-                                          subscription", receiver["id"])
+                        return test.FAIL("Node API Receiver {} subscription does not indicate an inactive " \
+                                         "subscription".format(receiver["id"]))
 
                 return test.PASS()
         except json.decoder.JSONDecodeError:
