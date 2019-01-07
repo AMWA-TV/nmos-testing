caa9a995db807aa848eb200500f527f437263077
Jasper peeters <jasper.peeters@riedel.net>
2019-01-07 12:15:54 +0100

    Added test websocket events

--- TestHelper.py
+++ TestHelper.py
@@ -16,6 +16,9 @@
 
 
 import requests
+from copy import copy
+import threading
+import websocket
 
 
 def ordered(obj):
@@ -52,3 +55,55 @@ def do_request(method, url, data=None):
         return False, str(e)
     except requests.exceptions.RequestException as e:
         return False, str(e)
+
+class WebsocketWorker(threading.Thread):
+    """Websocket Client Worker Thread"""
+
+    def __init__(self, ws_href):
+        """
+        Initializer
+        :param ws_href: websocket url (string)
+        """
+        threading.Thread.__init__(self, daemon=True)
+        self.ws_href = ws_href
+        self.ws = websocket.WebSocketApp(ws_href,
+                                         on_message=self.on_message,
+                                         on_close=self.on_close,
+                                         on_open=self.on_open,
+                                         on_error=self.on_error)
+        self.messages = list()
+        self.error_occured = False
+        self.error_message = ""
+
+    def run(self):
+        self.ws.run_forever()
+
+    def on_open(self):
+        pass
+
+    def on_message(self, message):
+        self.messages.append(message)
+
+    def on_close(self):
+        pass
+
+    def on_error(self, error):
+        self.error_occured = True
+        self.error_message = error
+
+    def close(self):
+        self.ws.close()
+
+    def get_messages(self):
+        msg_cpy = copy(self.messages)
+        self.clear_messages()  # Reset message list after reading
+        return msg_cpy
+
+    def did_error_occur(self):
+        return self.error_occured
+
+    def get_error_message(self):
+        return self.error_message
+
+    def clear_messages(self):
+        self.messages.clear()
