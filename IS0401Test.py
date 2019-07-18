8d247b209cd39455f9fb19cbbef9eef5ac1a59d9
Andrew Bonney <andrew.bonney@bbc.co.uk>
2019-07-18 09:23:03 +0100

    is0401: catch 'charset=utf-8' with a warning rather fail fail

--- IS0401Test.py
+++ IS0401Test.py
@@ -287,9 +287,18 @@ class IS0401Test(GenericTest):
 
         for resource in registry_data.posts:
             if "Content-Type" not in resource[1]["headers"]:
-                return test.FAIL("Node failed to signal its Content-Type correctly when registering.")
-            elif resource[1]["headers"]["Content-Type"] != "application/json":
-                return test.FAIL("Node signalled a Content-Type other than application/json.")
+                return test.FAIL("Node failed to signal its Content-Type when registering.")
+            else:
+                ctype_params = resource[1]["headers"]["Content-Type"].split(";")
+                if ctype_params[0] != "application/json":
+                    return test.FAIL("Node signalled a Content-Type of {} rather than application/json."
+                                     .format(resource[1]["headers"]["Content-Type"]))
+                elif len(ctype_params) == 2 and ctype_params[1].strip() == "charset=utf-8":
+                    return test.WARNING("Node signalled an unnecessary 'charset' in its Content-Type: {}"
+                                        .format(resource[1]["headers"]["Content-Type"]))
+                elif len(ctype_params) >= 2:
+                    return test.FAIL("Node signalled unexpected additional parameters in its Content-Type: {}"
+                                     .format(resource[1]["headers"]["Content-Type"]))
 
         return test.PASS()
 
