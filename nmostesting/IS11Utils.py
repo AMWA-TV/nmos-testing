cb497481aa4a3f8d38e93b7698e180871ca63bf5
Chris McArthur <prince.chrismc@gmail.com>
2021-09-13 19:43:54 -0400

    apply amwa copyright

--- nmostesting/IS11Utils.py
+++ nmostesting/IS11Utils.py
@@ -1,6 +1,4 @@
-# Copyright 2017 British Broadcasting Corporation
-#
-# Modifications Copyright 2018 Riedel Communications GmbH & Co. KG
+# Copyright (C) 2021 Advanced Media Workflow Association
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
