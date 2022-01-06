c1ad995e8e13ddf96d5a0838e10d35fb9a456661
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2022-01-06 17:45:38 +0000

    Validate subscription request can be satisfied by mock Registry

--- nmostesting/mocks/Registry.py
+++ nmostesting/mocks/Registry.py
@@ -589,10 +589,14 @@ def post_subscription(version):
     created = False
 
     try:
-        subscription, created = registry.subscribe_to_query_api(version, subscription_request["resource_path"])
-
         # Note: 'secure' not required in request, but is required in response
-        secure = subscription_request['secure'] if 'secure' in subscription_request else False
+        secure = subscription_request['secure'] if 'secure' in subscription_request else ENABLE_HTTPS
+
+        # Validate that this mock can satisfy the subscription request
+        if secure or subscription_request['max_update_rate_ms'] > 100 or len(subscription_request['params']) > 0:
+            abort(501)
+
+        subscription, created = registry.subscribe_to_query_api(version, subscription_request["resource_path"])
 
         subscription_response = {'id': subscription["id"],
                                  'max_update_rate_ms': subscription_request['max_update_rate_ms'],
