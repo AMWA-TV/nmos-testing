013849d6bb0a5d0ef8980b426f70de258c767dbf
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2024-09-09 15:23:01 +0100

    Add initial IS-14 tests

--- nmostesting/suites/IS1401Test.py
+++ nmostesting/suites/IS1401Test.py
@@ -13,8 +13,14 @@
 # limitations under the License.
 
 # from ..GenericTest import NMOSTestException
+from copy import copy
+from nmostesting.GenericTest import NMOSTestException
+from nmostesting.MS05Utils import NcBlock, NcBlockProperties
 from ..IS14Utils import IS14Utils
 from .MS0501Test import MS0501Test
+
+NODE_API_KEY = "node"
+CONFIGURATION_API_KEY = "configuration"
 
 
 class IS1401Test(MS0501Test):
@@ -22,10 +28,69 @@ class IS1401Test(MS0501Test):
     Runs Tests covering MS-05 and IS-14
     """
     def __init__(self, apis, **kwargs):
-        MS0501Test.__init__(self, apis, IS14Utils(apis), **kwargs)
+        self.is14_utils = IS14Utils(apis)
+        MS0501Test.__init__(self, apis, self.is14_utils, **kwargs)
+        self.node_url = apis[NODE_API_KEY]["url"]
+        self.configuration_url = apis[CONFIGURATION_API_KEY]["url"]
 
     def set_up_tests(self):
         super().set_up_tests()
 
     def tear_down_tests(self):
         super().tear_down_tests()
+
+    def test_01(self, test):
+        """Control Endpoint: Node under test advertises IS-14 control endpoint matching API under test"""
+        # https://specs.amwa.tv/is-14/branches/v1.0-dev/docs/IS-04_interactions.html
+
+        control_type = "urn:x-nmos:control:configuration/" + self.apis[CONFIGURATION_API_KEY]["version"]
+        return self.is14_utils.do_test_device_control(
+            test,
+            self.node_url,
+            control_type,
+            self.configuration_url,
+            self.authorization
+        )
+
+    def test_02(self, test):
+        """Role Path Syntax: Use the '.' character to delimit roles in role paths"""
+        
+        role_paths_endpoint = f"{self.configuration_url}rolePaths/"
+        
+        valid, response = self.do_request("GET", role_paths_endpoint)
+        
+        if not valid or response.status_code != 200:
+            test.FAIL("Failed to get roles")
+        
+        for role_path in response.json():
+            if role_path != "root" and not role_path.startswith("root."):
+                test.FAIL("Unexpected role path syntax.", "https://specs.amwa.tv/is-14/branches/"
+                          + f"{self.apis[CONFIGURATION_API_KEY]["spec_branch"]}"
+                          + "/docs/API_requests.html#url-and-usage")
+        return test.PASS()
+
+    def check_block_member_role_syntax(self, test, role_path):
+        """  Check syntax of roles in this block """         
+        response = self.is14_utils.get_property_value(test, NcBlockProperties.MEMBERS.value, role_path)
+         
+        for member in response:
+            # check the class descriptor schema
+            if "." in member["role"]:
+                raise NMOSTestException(test.FAIL(f"Illegal role syntax: {member["role"]}. "
+                                                  + "Roles must not contain a '.' character",
+                                                  "https://specs.amwa.tv/is-14/branches/"
+                                                  + f"{self.apis[CONFIGURATION_API_KEY]["spec_branch"]}"
+                                                  + "/docs/API_requests.html#url-and-usage"))
+            if self.is14_utils.is_block(member["classId"]):
+                child_role_path = copy(role_path)
+                child_role_path.append(member["role"])
+                self.check_block_member_role_syntax(test, child_role_path)
+            
+    
+    def test_03(self, test):
+        """Role Syntax: Check the `.` character is not be used in roles"""
+        # https://specs.amwa.tv/is-14/branches/v1.0-dev/docs/API_requests.html#url-and-usage
+
+        self.check_block_member_role_syntax(test, ["root"])
+        
+        return test.PASS()
