c04ba77b2861756b154aa0e4c17ccc25eb359d3c
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2025-02-21 14:52:21 +0000

    Enable running of auto tests in test suites derived from ControllerTest

--- nmostesting/suites/MS0501Test.py
+++ nmostesting/suites/MS0501Test.py
@@ -75,7 +75,7 @@ class MS0501Test(ControllerTest):
     def __init__(self, apis, utils, **kwargs):
         # Remove the RAML key to prevent this test suite from auto-testing IS-04 API
         apis[NODE_API_KEY].pop("raml", None)
-        ControllerTest.__init__(self, apis, **kwargs)
+        ControllerTest.__init__(self, apis, disable_auto=False, **kwargs)
         self.ms05_utils = utils
 
     def set_up_tests(self):
