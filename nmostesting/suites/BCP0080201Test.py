cb91f1b2b14f6236ece167260356205c5bf5b692
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2025-05-08 17:47:46 +0100

    Add status reporting delay test

--- nmostesting/suites/BCP0080201Test.py
+++ nmostesting/suites/BCP0080201Test.py
@@ -14,7 +14,8 @@
 
 
 from ..GenericTest import GenericTest, NMOSTestException
-from ..BCP008Utils import BCP008Utils
+from ..BCP008Utils import BCP008Utils, NcStatusMonitorProperties
+from ..MS05Utils import NcMethodStatus
 
 SENDER_MONITOR_API_KEY = "sendermonitor"
 NODE_API_KEY = "node"
@@ -50,6 +51,7 @@ class BCP0080201Test(GenericTest):
         apis[CONN_API_KEY].pop("raml", None)
         GenericTest.__init__(self, apis, omit_paths, **kwargs)
         self.bcp008_utils = BCP008Utils(apis)
+        self.sender_monitors = []
 
     def set_up_tests(self):
         self.bcp008_utils.reset()
@@ -71,6 +73,57 @@ class BCP0080201Test(GenericTest):
         # Clean up Websocket resources
         self.bcp008_utils.close_ncp_websocket()
 
+    def _status_ok(self, method_result):
+        if not hasattr(method_result, 'status'):
+            return False
+        return method_result.status == NcMethodStatus.OK \
+            or method_result.status == NcMethodStatus.PropertyDeprecated
+
+    def _get_sender_monitors(self, test):
+        if len(self.sender_monitors):
+            return self.sender_monitors
+
+        device_model = self.bcp008_utils.query_device_model(test)
+
+        self.sender_monitors = device_model.find_members_by_class_id(SENDER_MONITOR_CLASS_ID,
+                                                                     include_derived=True,
+                                                                     recurse=True,
+                                                                     get_objects=True)
+
+        return self.sender_monitors
+
     def test_01(self, test):
-        """Sender status monitoring"""
+        """Status reporting delay can be set to values within the published constraints"""
+        sender_monitors = self._get_sender_monitors(test)
+
+        if len(sender_monitors) == 0:
+            return test.UNCLEAR("No Receiver Monitors found in Device Model")
+
+        default_status_reporting_delay = 3
+        for monitor in sender_monitors:
+            method_result = self.bcp008_utils.set_property(
+                test, NcStatusMonitorProperties.STATUS_REPORTING_DELAY.value,
+                default_status_reporting_delay,
+                oid=monitor.oid, role_path=monitor.role_path)
+
+            if not self._status_ok(method_result):
+                return test.FAIL("SetProperty error: Error setting statusReportingDelay on monitor, "
+                                 f"oid={monitor.oid}, "
+                                 f"role path={monitor.role_path}")
+
+            method_result = self.bcp008_utils.get_property(
+                test, NcStatusMonitorProperties.STATUS_REPORTING_DELAY.value,
+                oid=monitor.oid, role_path=monitor.role_path)
+
+            if not self._status_ok(method_result):
+                return test.FAIL("GetProperty error: Error getting statusReportingDelay on monitor, "
+                                 f"oid={monitor.oid}, "
+                                 f"role path={monitor.role_path}")
+
+            if method_result.value != default_status_reporting_delay:
+                return test.FAIL("Unexpected status reporting delay on receiver monitor. "
+                                 f"Expected={default_status_reporting_delay} actual={method_result.value}, "
+                                 f"oid={monitor.oid}, "
+                                 f"role path={monitor.role_path}")
+
         return test.PASS()
