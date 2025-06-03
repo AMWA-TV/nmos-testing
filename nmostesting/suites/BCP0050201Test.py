feb153f053d6bbcdc433f7022cb3210a8bc683ae
Alain Bouchard <alabou_ca@gmail.com>
2025-06-03 13:00:57 +0000

    add AMWA copyright

--- nmostesting/suites/BCP0050201Test.py
+++ nmostesting/suites/BCP0050201Test.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2025 Matrox Graphics Inc.
+# Copyright (C) 2025 Advanced Media Workflow Association
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
